3f76817411b20cd80a6736b04ddbd0369896202c
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-26 12:22:16 -0500

    user signup routes tested

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 
 // gets all users form the database
 export const getAllUsers = async (
@@ -11,6 +12,24 @@ export const getAllUsers = async (
     const users = await User.find();
 
     return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword }); // encrypt the password before storing in the database
+    await user.save();
+
+    return res.status(200).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.log(error);
     return res.status(200).json({ message: "ERROR", cause: error.message });
