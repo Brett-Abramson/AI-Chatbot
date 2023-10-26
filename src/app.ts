b3d9ec576e1b764804fb578794b3a08905ae5184
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-26 11:23:18 -0500

    created routes/index.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import  express from "express";
 import { config } from "dotenv";
+import morgan from "morgan"
 
 config();
 const app = express();
-
+// MIDDLEWARES
 app.use(express.json());
+app.use(morgan("dev"))
 
 export default app;
