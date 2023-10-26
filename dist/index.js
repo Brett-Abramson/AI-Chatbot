a70f14c45086b487d17c6a043b62c3ab66bd1176
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-25 20:34:14 -0500

    basic index.ts completed. Ready for routes

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
-export {};
+import express from "express";
+const app = express();
+const PORT = 3000;
+// MIDDLEWARE
+app.use(express.json());
+// GET
+// PUT
+// POST
+// DELETE
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import  express from "express";
+
+const app = express();
+
+const PORT = 3000
+
+// MIDDLEWARE
+app.use(express.json());
+
+
+// GET
+// PUT
+// POST
+// DELETE
+
+
+app.listen(PORT, ()=>console.log(`listening on port ${PORT}`))
