0a562a234299c3d5867b3b5f30f5417ceaacd4da
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-26 11:26:09 -0500

    created routes/user.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import  express from "express";
+import express from "express";
 import { config } from "dotenv";
-import morgan from "morgan"
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 
 config();
 const app = express();
 // MIDDLEWARES
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(morgan("dev")); // remove this in production
+app.use("/api/v1", appRouter);
 
 export default app;
