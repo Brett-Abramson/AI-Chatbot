e4d471859cc41911a87405e9623229050b78fcf0
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-28 18:49:55 -0500

    using jwt to verify token in verifyToken function utils/token_manager.ts

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -63,6 +63,7 @@ export const userSignup = async (
   }
 };
 
+
 export const userLogin = async (
   req: Request,
   res: Response,
--- src/utils/token-manager.ts
+++ src/utils/token-manager.ts
@@ -16,6 +16,23 @@ export const verifyToken = async (
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.signedCookies[`${COOKIE_NAME}`]
-  console.log(token)
+  const token = req.signedCookies[`${COOKIE_NAME}`];
+  if(!token || token.trim()=== "") {
+    return res.status(401).json({message:"Token Not Received"})
+  }
+  return new Promise<void>((resolve, reject) => {
+    return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
+      if (err) {
+        reject(err.message);
+        return res.status(401).json({ message: "Token Expired" });
+      } else {
+        console.log("Token Verification Successful");
+        resolve();
+        // shares parameters with next function
+        res.locals.jwtData = success;
+        // go to next middleware
+        return next();
+      }
+    });
+  });
 };
